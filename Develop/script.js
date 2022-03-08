5a16bbf35d2dd043e207dffa2b90bf7b7d23dcb4
Thuy Nguyen <ttruong06@gmail.com>
2022-03-07 20:12:19 -0500

    start js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,8 @@
 // Assignment code here
-
-// code for Number Length
-var start = function () {
-  var length = window.prompt("How long do you want your password to be?  Choose between numbers 8 - 128.");
-    if (length < 8 || length > 128) {
-      window.alert("Length of password is not within the parameter.  Please try again.");
-      start();
-    }
-    else {
-      var char = window.confirm("Would you like your password to have special characters, such as '!, @, #, etc.?'");
-      var num = window.confirm("Would you like your password to contain numbers?");
-      var upper = window.confirm("Would you like your password to contain all upper case letters?");
-      var lower = window. confirm("Would you like your password to contain all lower case letters?");
-    }     
-}
-promptLength();
-
-var char = window.confirm("Would you like your password to have special characters, such as '!, @, #, etc.?'");
-var num = window.confirm("Would you like your password to contain numbers?");
-var upper = window.confirm("Would you like your password to contain all upper case letters?");
-var lower = window. confirm("Would you like your password to contain all lower case letters?");
-
-
-
-
+var numbers = "0123456789";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var symbols = "~!@#$%^&*()";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -39,3 +18,55 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  // function things
+  var all = "";
+
+  // this section builds the possible characters variable (all) based on user input
+  var length = parseInt(window.prompt("Pick a password length from 8 - 128."));
+  if(length <8 || length >128) {
+    alert("Password length is not between 8 - 128.");
+    location.reload();
+  }
+
+  if (window.confirm("Do you want numerical values for your password?")) {
+    // if user says ok, do something here, otherwise cancel and go to the next thing
+    all += numbers;
+
+  }
+
+  if (window.confirm("Do you want your password to contain uppercase letters?")) {
+    all += upper;
+  }
+
+  // lower
+  if (window.confirm("Do you want your password to contain lowercase letters?")) {
+    all += lower;
+  }
+
+  // symbols
+  if (window.confirm("Do you want your password to contain symbols?")) {
+    all += symbols;
+  }
+
+  console.log(all);
+
+  var allpw;
+  // if all is "a,b,c,d,e,f,g,h,i,j,1,2,3,4,5,6,7,8,9", all.charAt(math random here)
+  // build the password variable by adding a random character from all for all the number of times in length
+  for (var i = 0; i < length; i++) {
+    allpw += all.charAt(Math.floor(Math.random()) * all.length);
+    
+    // pick a random number from 0 to the length of the all variable
+    // take that character and put it in a password variable
+  }
+  return allpw; // eventually return the generated password here
+}
+
+
+
+
+  
+  
